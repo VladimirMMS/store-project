881475f93bc599360a86bd093dc80a37232a1f7a
unknown <vladimirmercado2001@gmail.com>
2021-11-30 15:18:22 -0500

    take out javascripts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,21 @@
+import fastify from "fastify";
+import initializeDatabase from "./db/config/connection";
 
+
+const server = fastify()
+
+server.get('/home', async (request, reply) => {
+    return "home"
+
+})
+
+
+server.listen(5000, (err, address) => {
+    initializeDatabase()
+    if(err) {
+        console.log(err)
+        process.exit(1)
+    }
+    console.log(`Server listening at ${address}`)
+})
 
