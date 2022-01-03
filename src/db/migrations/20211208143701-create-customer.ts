2ef2e893e85aa28a56516f852ab3be3585af6bde
unknown <vladimirmercado2001@gmail.com>
2022-01-03 08:52:11 -0500

    fix model code typing

--- src/db/migrations/20211208143701-create-customer.ts
+++ src/db/migrations/20211208143701-create-customer.ts
@@ -1,33 +1,36 @@
+/* eslint-disable linebreak-style */
 'use strict';
 module.exports = {
-  up: async (queryInterface:any, Sequelize:any) => {
-    await queryInterface.createTable('Customers', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      name: {
-        type: Sequelize.STRING
-      },
-      lastname: {
-        type: Sequelize.STRING
-      },
-      age: {
-        type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: async (queryInterface:any, Sequelize:any) => {
-    await queryInterface.dropTable('Customers');
-  }
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	up: async (queryInterface:any, Sequelize:any) => {
+		await queryInterface.createTable('Customers', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER
+			},
+			name: {
+				type: Sequelize.STRING
+			},
+			lastname: {
+				type: Sequelize.STRING
+			},
+			age: {
+				type: Sequelize.INTEGER
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			}
+		});
+	},
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	down: async (queryInterface:any, Sequelize:any) => {
+		await queryInterface.dropTable('Customers');
+	}
 };
--- src/db/migrations/20211209181533-create-product.ts
+++ src/db/migrations/20211209181533-create-product.ts
@@ -1,32 +1,35 @@
+/* eslint-disable linebreak-style */
 'use strict';
 module.exports = {
-  up: async (queryInterface:any, Sequelize:any) => {
-    await queryInterface.createTable('Products', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      name: {
-        type: Sequelize.STRING
-      },
-      price: {
-        type: Sequelize.INTEGER,
-        allowNull: false
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	up: async (queryInterface:any, Sequelize:any) => {
+		await queryInterface.createTable('Products', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER
+			},
+			name: {
+				type: Sequelize.STRING
+			},
+			price: {
+				type: Sequelize.INTEGER,
+				allowNull: false
 
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: async (queryInterface:any, Sequelize:any) => {
-    await queryInterface.dropTable('Products');
-  }
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			}
+		});
+	},
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	down: async (queryInterface:any, Sequelize:any) => {
+		await queryInterface.dropTable('Products');
+	}
 };
