608e48cf7b44dd86575291479593f35046c71311
unknown <vladimirmercado2001@gmail.com>
2022-01-04 14:15:15 -0500

    fix pretter config

--- src/db/models/index.ts
+++ src/db/models/index.ts
@@ -1,46 +1,46 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable prettier/prettier */
+
 'use strict';
 import dotenv from 'dotenv';
 import fs from 'fs';
 import path from 'path';
-import {Sequelize} from 'sequelize';
+import { Sequelize } from 'sequelize';
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
 import config from '../config/config';
 
 dotenv.config();
-const db:any = {};
+const db: any = {};
 let sequelize: any;
 
-// eslint-disable-next-line prefer-const
 
-// eslint-disable-next-line @typescript-eslint/no-empty-function
 function getModels() {
+  sequelize = new Sequelize(
+    config.development.database,
+    config.development.username,
+    config.development.password,
+    {
+      dialect: 'postgres'
+    }
+  );
+  fs.readdirSync(__dirname)
+    .filter((file: string) => {
+      return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.ts';
+    })
+    .map(async (file: any) => {
+      const { default: models } = await import(path.join(__dirname, file));
+      const model = models(sequelize, Sequelize);
+      db[model.name] = model;
+    });
 
-	sequelize = new Sequelize(config.development.database, config.development.username, config.development.password, {
-		dialect: 'postgres'
-	});
-	fs
-		.readdirSync(__dirname)
-		.filter((file: string) => {
-			return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.ts');
-		})
-		.map(async (file:any) => {
-			const {default: models} = await import(path.join(__dirname, file)); 
-			const model = models(sequelize, Sequelize);
-			db[model.name] = model;
-		});
-	
-	Object.keys(db).forEach(modelName => {
-		if (db[modelName].associate) {
-			db[modelName].associate(db);
-		}
-	});
+  Object.keys(db).forEach((modelName) => {
+    if (db[modelName].associate) {
+      db[modelName].associate(db);
+    }
+  });
 
-	db.sequelize = sequelize;
-	db.Sequelize = Sequelize;
-	return db;
-	
+  db.sequelize = sequelize;
+  db.Sequelize = Sequelize;
+  return db;
 }
 
 export default getModels;
--- src/route/product/route.ts
+++ src/route/product/route.ts
@@ -1,39 +1,36 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { 
-	getAllProduct, 
-	getAllProductById, 
-	createProduct, 
-	updateProduct, 
-	deleteProducttById} 
-	from './controller';
-
+import {
+  getAllProduct,
+  getAllProductById,
+  createProduct,
+  updateProduct,
+  deleteProducttById
+} from './controller';
 
 const option = {
-	schema: {
-		querystring: {
-			customer: { type: 'string' },
-			excitement: { type: 'string' }
-		},
-		response: {
-			200: {
-				type: 'object',
-				properties: {
-					data: { type: 'object' }
-				}
-			}
-		}
-	},
+  schema: {
+    querystring: {
+      customer: { type: 'string' },
+      excitement: { type: 'string' }
+    },
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          data: { type: 'object' }
+        }
+      }
+    }
+  }
 };
-  
-function productRoute(fastify:any, opt:any, done:any) {
-	fastify.get('/product', option.schema.response,getAllProduct);
-	fastify.get('/product/:id',option.schema.response,getAllProductById);
-	fastify.post('/product', option.schema.querystring.customer, createProduct);
-	fastify.put('/product/:id', updateProduct);
-	fastify.delete('/product/:id', deleteProducttById);
-	done();
-    
+
+function productRoute(fastify: any, opt: any, done: any) {
+  fastify.get('/product', option.schema.response, getAllProduct);
+  fastify.get('/product/:id', option.schema.response, getAllProductById);
+  fastify.post('/product', option.schema.querystring.customer, createProduct);
+  fastify.put('/product/:id', updateProduct);
+  fastify.delete('/product/:id', deleteProducttById);
+  done();
 }
-  
+
 export default productRoute;
-  
