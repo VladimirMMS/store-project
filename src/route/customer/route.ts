31e9cf4959c334e3e832aefe21e19189417f9c9a
unknown <vladimirmercado2001@gmail.com>
2022-01-06 13:31:20 -0500

    implement somethings

--- src/route/customer/route.ts
+++ src/route/customer/route.ts
@@ -7,27 +7,10 @@ import {
   deleteCustomerById
 } from './controller';
 
-const option = {
-  schema: {
-    querystring: {
-      customer: { type: 'string' },
-      excitement: { type: 'string' }
-    },
-    response: {
-      200: {
-        type: 'object',
-        properties: {
-          data: { type: 'object' }
-        }
-      }
-    }
-  }
-};
-
 export default (fastify: FastifyInstance, opt: any, done: any) => {
-  fastify.get('/customer', option, getAllCustomer);
-  fastify.get('/customer/:id', option, getAllCustomerById);
-  fastify.post('/customer', option, createCustomer);
+  fastify.get('/customer', getAllCustomer);
+  fastify.get('/customer/:id', getAllCustomerById);
+  fastify.post('/customer', createCustomer);
   fastify.put('/customer/:id', updateCustomer);
   fastify.delete('/customer/:id', deleteCustomerById);
   done();
--- src/route/product/route.ts
+++ src/route/product/route.ts
@@ -7,27 +7,10 @@ import {
 } from './controller';
 import { FastifyInstance } from 'fastify';
 
-const option = {
-  schema: {
-    querystring: {
-      customer: { type: 'string' },
-      excitement: { type: 'string' }
-    },
-    response: {
-      200: {
-        type: 'object',
-        properties: {
-          data: { type: 'object' }
-        }
-      }
-    }
-  }
-};
-
 export default (fastify: FastifyInstance, opt: any, done: any) => {
-  fastify.get('/product', option, getAllProduct);
-  fastify.get('/product/:id', option, getAllProductById);
-  fastify.post('/product', option, createProduct);
+  fastify.get('/product', getAllProduct);
+  fastify.get('/product/:id', getAllProductById);
+  fastify.post('/product', createProduct);
   fastify.put('/product/:id', updateProduct);
   fastify.delete('/product/:id', deleteProducttById);
   done();
