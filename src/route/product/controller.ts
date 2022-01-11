8f567598fc01c8c38b9a6c573b05408b5ce6da65
unknown <vladimirmercado2001@gmail.com>
2022-01-11 09:41:38 -0500

    create class

--- src/route/product/controller.ts
+++ src/route/product/controller.ts
@@ -1,68 +1,73 @@
 import getModels from '../../db/models/index';
+import { ProductAttribute } from '../../interfaces';
 
-export async function createProduct(request: any) {
-  const db = await getModels();
-  const { Product } = db;
+export default class ProductCrud {
+  constructor(private serviceModel: ProductAttribute) {}
 
-  try {
-    const newProduct = await Product.create(request.body);
-    return newProduct;
-  } catch (error: any) {
-    return { error: error.message };
+  async createProduct(request: any) {
+    const db = await getModels();
+    const { Product } = db;
+
+    try {
+      const newProduct = await Product.create(request.body);
+      return newProduct;
+    } catch (error: any) {
+      return { error: error.message };
+    }
   }
-}
 
-export async function getAllProduct() {
-  const db = await getModels();
-  const { Product } = db;
-  try {
-    const product = await Product.findAll();
-    return product;
-  } catch (error: any) {
-    return { error: error.message };
+  async getAllProduct() {
+    const db = await getModels();
+    const { Product } = db;
+    try {
+      const product = await Product.findAll();
+      return product;
+    } catch (error: any) {
+      return { error: error.message };
+    }
   }
-}
 
-export async function getAllProductById(request: any) {
-  const db = await getModels();
-  const { Product } = db;
+  async getAllProductById(request: any) {
+    const db = await getModels();
+    const { Product } = db;
 
-  try {
-    const product = await Product.findOne({
-      where: parseInt(request.params.id)
-    });
-    return product;
-  } catch (error: any) {
-    return { error: error.message };
+    try {
+      const product = await Product.findOne({
+        where: parseInt(request.params.id)
+      });
+      return product;
+    } catch (error: any) {
+      return { error: error.message };
+    }
   }
-}
 
-export async function updateProduct(request: any) {
-  const db = await getModels();
-  const { Product } = db;
+  async updateProduct(request: any) {
+    const db = await getModels();
+    const { Product } = db;
 
-  try {
-    const updatedProduct = await Product.update(request.body, {
-      where: { id: request.params.id }
-    });
+    try {
+      const updatedProduct = await Product.update(request.body, {
+        where: { id: request.params.id }
+      });
 
-    return updatedProduct;
-  } catch (error: any) {
-    return { error: error.message };
+      return updatedProduct;
+    } catch (error: any) {
+      return { error: error.message };
+    }
   }
-}
 
-export async function deleteProducttById(request: any) {
-  const db = await getModels();
-  const { Product } = db;
-  try {
-    const product = await Product.destroy({
-      where: {
-        id: request.params.id
-      }
-    });
-    return product;
-  } catch (error: any) {
-    return { error: error.message };
+  async deleteProducttById(request: any) {
+    const db = await getModels();
+    const { Product } = db;
+    try {
+      const product = await Product.destroy({
+        where: {
+          id: request.params.id
+        }
+      });
+      return product;
+    } catch (error: any) {
+      return { error: error.message };
+    }
   }
 }
