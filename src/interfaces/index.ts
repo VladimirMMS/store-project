a49e9d5a7574bfab11565f90be24b763a8f09f2f
unknown <vladimirmercado2001@gmail.com>
2022-01-11 09:40:46 -0500

    add interfaces

---
+++ src/interfaces/index.ts
@@ -0,0 +1,12 @@
+export interface CustomerAttribute {
+  id: string;
+  name: string;
+  lastname: string;
+  age: number;
+}
+
+export interface ProductAttribute {
+  id: number;
+  name: string;
+  price: number;
+}
