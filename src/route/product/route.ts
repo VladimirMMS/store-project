f0dfa03d1b18df75e82886529fcb51d733100ba4
unknown <vladimirmercado2001@gmail.com>
2022-01-14 09:39:07 -0500

    add class

--- src/route/product/route.ts
+++ src/route/product/route.ts
@@ -1,17 +1,15 @@
-import {
-  getAllProduct,
-  getAllProductById,
-  createProduct,
-  updateProduct,
-  deleteProducttById
-} from './controller';
 import { FastifyInstance } from 'fastify';
+import { ProductCrud } from './controller';
+import getModels from '../../db/models';
 
 export default (fastify: FastifyInstance, opt: any, done: any) => {
-  fastify.get('/product', getAllProduct);
-  fastify.get('/product/:id', getAllProductById);
-  fastify.post('/product', createProduct);
-  fastify.put('/product/:id', updateProduct);
-  fastify.delete('/product/:id', deleteProducttById);
+  const db = getModels();
+  const { Product } = db;
+  const product = new ProductCrud(Product);
+  fastify.get('/product', () => product.getAllProduct());
+  fastify.get('/product/:id', (request) => product.getAllProductById(request));
+  fastify.post('/product', (request) => product.createProduct(request));
+  fastify.put('/product/:id', (request) => product.updateProduct(request));
+  fastify.delete('/product/:id', (request) => product.deleteProducttById(request));
   done();
 };
