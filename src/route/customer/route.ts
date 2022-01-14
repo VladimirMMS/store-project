aad4de26ce12ac1a0220122e877f9d3b175e4ecf
unknown <vladimirmercado2001@gmail.com>
2022-01-14 09:38:31 -0500

    customer function

--- src/route/customer/route.ts
+++ src/route/customer/route.ts
@@ -6,8 +6,11 @@ import {
   updateCustomer,
   deleteCustomerById
 } from './controller';
+import getModels from '../../db/models';
 
 export default (fastify: FastifyInstance, opt: any, done: any) => {
+  const db = getModels();
+  const { Product } = db;
   fastify.get('/customer', getAllCustomer);
   fastify.get('/customer/:id', getAllCustomerById);
   fastify.post('/customer', createCustomer);
