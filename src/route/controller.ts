000ba9b8440efba94948de07dfd55c7f177b44f5
unknown <vladimirmercado2001@gmail.com>
2022-01-18 12:48:52 -0500

    implement somethin of feedback

--- src/route/controller.ts
+++ src/route/controller.ts
@@ -11,47 +11,32 @@ export class DefaultController {
   }
 
   async getAllService() {
-    try {
-      const product = await this.model.findAll();
-      return product;
-    } catch (error: any) {
-      throw new Error(error);
-    }
+    const product = await this.model.findAll();
+    return product;
   }
 
   async getAllServiceById(request: any) {
-    try {
-      const product = await this.model.findOne({
-        where: parseInt(request.params.id)
-      });
-      return product;
-    } catch (error: any) {
-      throw new Error(error);
-    }
+    const product = await this.model.findOne({
+      where: parseInt(request.params.id)
+    });
+    return product;
   }
 
   async updateService(request: any) {
-    try {
-      const updatedProduct = await this.model.update(request.body, {
-        where: { id: request.params.id }
-      });
+    const updatedProduct = await this.model.update(request.body, {
+      where: { id: request.params.id }
+    });
 
-      return updatedProduct;
-    } catch (error: any) {
-      throw new Error(error);
-    }
+    return updatedProduct;
   }
 
   async deleteServiceById(request: any) {
-    try {
-      const product = await this.model.destroy({
-        where: {
-          id: request.params.id
-        }
-      });
-      return product;
-    } catch (error: any) {
-      throw new Error(error);
-    }
+    const product = await this.model.destroy({
+      where: {
+        id: request.params.id
+      }
+    });
+
+    return product;
   }
 }
