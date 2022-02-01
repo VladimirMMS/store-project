dee8540c886372a0c4befb391bffcece1a4028d8
unknown <vladimirmercado2001@gmail.com>
2022-02-01 09:56:23 -0500

    fix some problem

---
+++ src/route/route.ts
@@ -0,0 +1,12 @@
+export class DefaultRoute {
+  static createRoute: any;
+
+  createRoute(fastify: any, option: any, done: any) {
+    fastify.get('/', () => option.controller.getAllService());
+    fastify.get('/:id', (request: any) => option.controller.getAllServiceById(request));
+    fastify.post('/', (request: any) => option.controller.createService(request));
+    fastify.put('//:id', (request: any) => option.controller.updateService(request));
+    fastify.delete('//:id', (request: any) => option.controller.deleteServiceById(request));
+    done();
+  }
+}
