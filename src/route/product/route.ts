996db0d7dc7f8c9c1493e5a9e163fa896e5a78b4
unknown <vladimirmercado2001@gmail.com>
2022-02-21 16:03:33 -0500

    optimizing

--- src/route/product/route.ts
+++ src/route/product/route.ts
@@ -1,11 +1,10 @@
 import { DefaultRoute } from '../route';
 
 export class ProductRoute extends DefaultRoute {
-  static productRoute(fastify: any, option: any, done: any) {
+  static createRoute(fastify: any, option: any, done: any) {
     fastify.get('/category/:category', (request: any) =>
       option.controller.getProductByCategory(request)
     );
-    done();
     super.createRoute(fastify, option, done);
   }
 }
