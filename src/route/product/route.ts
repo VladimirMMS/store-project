5a400bc0e59d6dcf1e0698b7c9fbb0dfeab1d015
unknown <vladimirmercado2001@gmail.com>
2022-02-23 10:42:05 -0500

    modify export for default

--- src/route/product/route.ts
+++ src/route/product/route.ts
@@ -1,6 +1,6 @@
-import { DefaultRoute } from '../route';
+import DefaultRoute from '../route';
 
-export class ProductRoute extends DefaultRoute {
+export default class ProductRoute extends DefaultRoute {
   static createRoute(fastify: any, option: any, done: any) {
     fastify.get('/category/:category', (request: any) =>
       option.controller.getProductByCategory(request)
