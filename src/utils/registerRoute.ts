671108afaab9552a3b1bcad8e1ec92d8157ae8b2
unknown <vladimirmercado2001@gmail.com>
2022-02-23 10:43:09 -0500

    fix some problem

--- src/utils/registerRoute.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyInstance } from 'fastify';
-
-export async function registerRoute(fastify: FastifyInstance, option: any, done: any) {
-  const { route } = option;
-  route.default.createRoute(fastify, option, done);
-  route.product.productRoute(fastify, option, done);
-
-  done();
-}
