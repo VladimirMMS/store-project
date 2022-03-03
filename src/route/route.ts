37743e9dc01b313d7a859dc681c1065e2ac58ee7
unknown <vladimirmercado2001@gmail.com>
2022-03-03 09:05:10 -0500

    modify route

--- src/route/route.ts
+++ src/route/route.ts
@@ -2,7 +2,7 @@ export default class DefaultRoute {
   static createRoute(fastify: any, option: any, done: any) {
     fastify.get('/', () => option.controller.getAllService());
     fastify.get('/:id', (request: any) => option.controller.getAllServiceById(request));
-    fastify.post('/', (request: any) => option.controller.createService(request));
+    fastify.post('/', (request: any) => option.controller.createService(request.body));
     fastify.put('//:id', (request: any) => option.controller.updateService(request));
     fastify.delete('//:id', (request: any) => option.controller.deleteServiceById(request));
     done();
