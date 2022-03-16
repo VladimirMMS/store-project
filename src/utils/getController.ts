56ec9ef02c7ccde71135e32d8963748a4e0f5cec
unknown <vladimirmercado2001@gmail.com>
2022-03-16 10:39:51 -0400

    add init()

--- src/utils/getController.ts
+++ src/utils/getController.ts
@@ -1,11 +1,12 @@
 import path from 'path';
-import { getModel } from '../db/models';
+import initDb, { getModel } from '../db/models';
 
 function capitalize(str: string) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
 export async function getController(modelName: string, controllerPath: string) {
+  await initDb();
   const model = await getModel(capitalize(modelName));
   const { default: controller } = await import(path.resolve(controllerPath));
   return new controller(model);
