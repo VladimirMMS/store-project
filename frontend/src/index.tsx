c20f4eb79cd724518492b4cb279efef6e0911fd8
Vladimir Mercado <vladimirmercado2001@gmail.com>
2022-03-31 11:40:15 -0400

    doing some config

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+// eslint-disable-next-line import/extensions
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
