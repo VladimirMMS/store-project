a3f39532073f7a0df5b9d3e347472af5198d37f3
Vladimir Mercado <vladimirmercado2001@gmail.com>
2022-04-22 10:44:11 -0400

    add theme

---
+++ frontend/src/theme.ts
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material';
+import { red } from '@mui/material/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: red[500]
+    }
+  }
+});
+
+export default theme;
