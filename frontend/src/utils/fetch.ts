58bb35e6aaee605e67c5225bb3430fdec4191674
Vladimir Mercado <vladimirmercado2001@gmail.com>
2022-04-27 15:18:47 -0400

    fetch

--- frontend/src/utils/fetch.ts
+++ frontend/src/utils/fetch.ts
@@ -1,16 +1,23 @@
-import dotenv from 'dotenv'
-
-
 export const { REACT_APP_BACKEND } = process.env;
 
-export class EndpointRequest{
-    static apiStore = REACT_APP_BACKEND
+export class EndpointRequest {
+  static apiStore = REACT_APP_BACKEND;
 
-    get(url:string) {
-        const bodyOpts = {
-            method: 'GET',
-          };
-          return fetch(`${EndpointRequest.apiStore}${url}`, bodyOpts);
-    }
+  get(url: string) {
+    const bodyOpts = {
+      method: 'GET'
+    };
+    return fetch(`${EndpointRequest.apiStore}${url}`, bodyOpts);
+  }
 
+  post(url: string, body: object) {
+    const bodyOpts = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    };
+    return fetch(`${EndpointRequest.apiStore}${url}`, bodyOpts)
+  }
 }
