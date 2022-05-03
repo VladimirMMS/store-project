c674b78363983cfa444a23aa76939c9ee74ecaa3
Vladimir Mercado <vladimirmercado2001@gmail.com>
2022-05-03 15:15:35 -0400

    fix some problem

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -3,16 +3,12 @@ import thunk from 'redux-thunk';
 import { reducer } from '../reducers';
 
 declare global {
-    interface Window {
-      __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
-    }
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
   }
+}
 
 const composeEnhancers =
-  (typeof window !== "undefined" &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+  (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
-export const store = createStore(reducer,
-    composeEnhancers(applyMiddleware(thunk))
-    );
-
+export const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
